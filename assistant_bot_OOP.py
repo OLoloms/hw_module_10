ff73f5e2f80d708c0a1cd40a6c596b651be10523
OLoloms <prostotsapkov@gmail.com>
2023-06-15 18:25:34 +0300

    Update assistant_bot_OOP.py
    Checks passed

--- assistant_bot_OOP.py
+++ assistant_bot_OOP.py
@@ -2,31 +2,33 @@ from collections import UserDict
 
 class AdressBook(UserDict):
     def add_record(self, record):
-        self[record.name] = record.list_phone
-    def show_data(self):
-        return self.data
+        self.data[record.name.name] = record
 
 class Record:
-    def set_name(self, name):
-        self.name = name.name
-    def set_phone(self, *phones):
-        self.list_phone = []
-        for phone in phones:
-            self.list_phone.append(phone.phone)
-        
-    def change_phone(self, previous_phone, new_phone):
-        index = self.list_phone.index(previous_phone.phone)
-        self.list_phone[index] = new_phone.phone
-    def add_phone(self, phone):
-        self.list_phone.append(phone.phone)
-    def delete_phone(self, phone):
-        self.list_phone.remove(phone.phone)
+    def __init__(self, name, *field) -> None:
+        self.name = name
+        self.field = list(field) or []
 
 class Field:
     pass
+
 class Name:
     def __init__(self, name) -> None:
         self.name = name
+
 class Phone:
-    def __init__(self, phone) -> None:
-        self.phone = phone
+    def __init__(self, phone=None) -> None:
+        self.value = phone
+
+if __name__ == "__main__":
+    name = Name('Bill')
+    phone = Phone('1234567890')
+    rec = Record(name, phone)
+    ab = AdressBook()
+    ab.add_record(rec)
+    assert isinstance(ab['Bill'], Record)
+    assert isinstance(ab['Bill'].name, Name)
+    assert isinstance(ab['Bill'].field, list)
+    assert isinstance(ab['Bill'].field[0], Phone)
+    assert ab['Bill'].field[0].value == '1234567890'
+    print('All Ok)')
